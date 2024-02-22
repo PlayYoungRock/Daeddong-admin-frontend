cbf070f6267fb0b08b62e67859742d114cd400b1
SeonjaeLee <tjswo9502@gmail.com>
2024-02-22 21:48:38 +0900

    :sparkles: 라디오 컴포넌트 구현

---
+++ src/components/Radio/Radio.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Inactive from '@assets/inactive-radio.svg?react';
+import Active from '@assets/active-radio.svg?react';
+import { Text } from '../Text';
+
+export const Radio = ({ options, className, ...props }) => {
+  return (
+    <Container className={className}>
+      {options.map(({ label, value }, i) => (
+        <Label key={`radio-${props.name}-${label}-${value}-${i}`}>
+          {props.value === value ? <Active /> : <Inactive />}
+          <Text>{label}</Text>
+          <UnVisibleInput
+            {...props}
+            type="radio"
+            value={value}
+          ></UnVisibleInput>
+        </Label>
+      ))}
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  gap: 8px;
+`;
+
+const Label = styled.label`
+  display: flex;
+  gap: 8px;
+  align-items: center;
+`;
+
+const UnVisibleInput = styled.input`
+  display: none;
+`;
