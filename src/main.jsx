d0dd28cbf00d8c14dc8d53212327d22626f8025a
SeonjaeLee <tjswo9502@gmail.com>
2024-02-25 23:58:18 +0900

    :sparkles: react query client추가

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,14 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import '@styles/reset.css';
 import { router } from './routes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={createBrowserRouter(router)} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={createBrowserRouter(router)} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
