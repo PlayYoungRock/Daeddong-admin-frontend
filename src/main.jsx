651ff90d1d726e212d8f3c0de99eec2a73d1529e
SeonjaeLee <tjswo9502@gmail.com>
2024-03-03 00:34:11 +0900

    :sparkles: dev tools 추가

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import '@styles/reset.css';
 import { router } from './routes';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={createBrowserRouter(router)} />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>,
 );
