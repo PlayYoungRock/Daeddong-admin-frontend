e1f9544072a3d4f8dfaa679e44e5748ac0a654ee
SeonjaeLee <tjswo9502@gmail.com>
2024-03-03 00:34:41 +0900

    :recycle: query string 추가

--- src/utils/toiletAPI.js
+++ src/utils/toiletAPI.js
@@ -1,6 +1,13 @@
 import { Http } from './http';
+import queryString from 'query-string';
 
-export const getToiletList = async () => {
-  const { data } = await Http.get('toiletList');
-  return data.toiletList;
+/**
+ * @returns {Object}
+ * @property {number} totalCount
+ * @property {Array<Object>} toiletList
+ */
+export const getToiletList = async (query) => {
+  const { data } = await Http.get(`toiletList?${queryString.stringify(query)}`);
+
+  return data;
 };
