7ab4bd828c56b80f18a8799ba22cab306a8ec805
SeonjaeLee <tjswo9502@gmail.com>
2024-03-03 23:46:02 +0900

    :sparkles: 시군구 api 추가

--- src/utils/toiletAPI.js
+++ src/utils/toiletAPI.js
@@ -1,13 +1,27 @@
 import { Http } from './http';
 import queryString from 'query-string';
 
+export const TOILET_LIST = 'toiletList';
+export const SI_GUN_GU_LIST = 'sigunguList';
+
 /**
  * @returns {Object}
  * @property {number} totalCount
  * @property {Array<Object>} toiletList
  */
 export const getToiletList = async (query) => {
-  const { data } = await Http.get(`toiletList?${queryString.stringify(query)}`);
+  const { data } = await Http.get(
+    `${TOILET_LIST}?${queryString.stringify(query)}`,
+  );
 
   return data;
+};
+
+/**
+ * @description 시군구 api 리소스가 부족하여 서울시의 군구만 받도록한다.
+ */
+export const getGunguList = async () => {
+  const { data } = await Http.get(`${SI_GUN_GU_LIST}`);
+
+  return data.sigunguList;
 };
