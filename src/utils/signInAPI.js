a97ace78d8cc0c7c34bdec45f267917d358acab6
SeonjaeLee <tjswo9502@gmail.com>
2024-03-13 23:50:36 +0900

    :recycle: 토큰관련 상수 추가

--- src/utils/signInAPI.js
+++ src/utils/signInAPI.js
@@ -1,9 +1,10 @@
 import { Http } from './http';
 
+export const ADMIN_TOKEN = 'admin-token';
 export const SIGN_IN = 'login';
 
 export const postSignIn = async (form) => {
-  const data = await Http.post(SIGN_IN, form);
+  const { data } = await Http.post(SIGN_IN, form);
 
   return data;
 };
