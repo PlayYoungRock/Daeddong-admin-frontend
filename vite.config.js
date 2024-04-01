59b906d0d087db8f1fee87e74f52e8fe09ac7f49
SeonjaeLee <tjswo9502@gmail.com>
2024-04-01 23:34:24 +0900

    :wrench: https 제거

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
-import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), mkcert()],
+  plugins: [react(), svgr()],
   server: {
     port: 3001,
   },
