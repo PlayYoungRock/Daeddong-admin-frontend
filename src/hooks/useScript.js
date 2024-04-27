78ed712bf1ffb4e4baaa168c7c4b12ae4d252de7
SeonjaeLee <tjswo9502@gmail.com>
2024-04-27 15:16:48 +0900

    :recycle: script 전역변수를 두어 모든 컴포넌트에서 script 유무 확인

--- src/hooks/useScript.js
+++ src/hooks/useScript.js
@@ -1,31 +1,81 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 
-export const useScript = (src) => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+const scripts = {};
+
+const isBrowser = typeof window !== 'undefined' && typeof window.document !== 'undefined';
+
+const getScriptFromDom = (src) => {
+  const element = document.querySelector(`script[src="${src}"]`);
+  if (!element) return;
+
+  return (scripts[src] = {
+    loading: false,
+    error: null,
+    scriptEl: element,
+  });
+};
+
+export const useScript = ({ src, checkForExisting = false, ...attributes }) => {
+  let status = src ? scripts[src] : undefined;
+  if (isBrowser && src && checkForExisting && !status) {
+    status = getScriptFromDom(src);
+  }
+
+  const [loading, setLoading] = useState(status ? status.loading : Boolean(src));
+  const [error, setError] = useState(status ? status.error : null);
+
+  const [scriptLoaded, setScriptLoaded] = useState(false);
 
   useEffect(() => {
-    let script = document.querySelector(`script[src="${src}"]`);
+    if (!isBrowser || !src || !loading || error || scriptLoaded) return;
 
-    if (!script) {
-      script = document.createElement('script');
-      script.src = src;
-      script.async = true;
+    status = scripts[src];
+    if (!status && checkForExisting) {
+      status = getScriptFromDom(src);
     }
 
-    const handleLoad = () => setIsLoading(false);
-    const handleError = (error) => setError(error);
+    let scriptEl;
+    if (status) {
+      scriptEl = status.scriptEl;
+    } else {
+      scriptEl = document.createElement('script');
+      scriptEl.src = src;
 
-    script.addEventListener('load', handleLoad);
-    script.addEventListener('error', handleError);
+      Object.keys(attributes).forEach((key) => {
+        if (scriptEl[key] === undefined) {
+          scriptEl.setAttribute(key, attributes[key]);
+        } else {
+          scriptEl[key] = attributes[key];
+        }
+      });
 
-    document.body.appendChild(script);
+      status = scripts[src] = {
+        loading: true,
+        error: null,
+        scriptEl: scriptEl,
+      };
+    }
+
+    const handleLoad = () => {
+      if (status) status.loading = false;
+      setLoading(false);
+      setScriptLoaded(true);
+    };
+    const handleError = (error) => {
+      if (status) status.error = error;
+      setError(error);
+    };
+
+    scriptEl.addEventListener('load', handleLoad);
+    scriptEl.addEventListener('error', handleError);
+
+    document.body.appendChild(scriptEl);
 
     return () => {
-      script.removeEventListener('load', handleLoad);
-      script.removeEventListener('error', handleError);
+      scriptEl.removeEventListener('load', handleLoad);
+      scriptEl.removeEventListener('error', handleError);
     };
   }, [src]);
 
-  return { isLoading, error };
+  return [loading, error];
 };
